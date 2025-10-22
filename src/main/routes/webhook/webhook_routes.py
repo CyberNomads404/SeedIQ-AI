5656970cb63c0e9f57b27ea6cc61115380f48c5e
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2025-10-22 16:26:54 -0300

    feat: :sparkles: Adicionar rotas para enfileirar jobs e obter status com autenticação de webhook

--- src/main/routes/webhook/webhook_routes.py
+++ src/main/routes/webhook/webhook_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src.middlewares.webhook_auth import webhook_auth_required
+from src.views.http_types.http_request import HttpRequest
+from src.controllers.job_controller import enqueue
 
 webhook_routes_bp = Blueprint('webhook_routes', __name__)
 
@@ -8,8 +10,17 @@ webhook_routes_bp = Blueprint('webhook_routes', __name__)
 def webhook():
     return jsonify({"status": True, "message": "Webhook authenticated successfully"}), 200
 
-@webhook_routes_bp.route('/test', methods=['POST'])
+@webhook_routes_bp.route('/enqueue', methods=['POST'])
 @webhook_auth_required
-def webhook_test():
+def webhook_enqueue():
+    http_request = HttpRequest(body=request.json)
+    http_response = enqueue(http_request)
     
+    return jsonify(http_response.body), http_response.status_code
 
+@webhook_routes_bp.route('/get_status', methods=['GET'])
+def webhook_get_status():
+    http_request = HttpRequest(query=request.args)
+    http_response = enqueue(http_request)
+    
+    return jsonify(http_response.body), http_response.status_code
